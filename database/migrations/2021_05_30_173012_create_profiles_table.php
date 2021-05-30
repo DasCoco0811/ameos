6c73991c52034b76df84c00529667e08600e2c6d
DasCoco0811 <mck.dascoco@gmail.com>
2021-05-30 19:31:12 +0200

    Nofitications, PRofile

--- database/migrations/2021_05_30_173012_create_profiles_table.php
+++ database/migrations/2021_05_30_173012_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationsTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('source_id');
-            $table->string('title', 32);
-            $table->string('icon');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateNotificationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('profiles');
     }
 }
